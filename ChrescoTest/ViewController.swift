d01e5c13ff3b3a0d66a7303dd3631786de82dd1f
Miguel <iscmiguelsamaniego@gmail.com>
2018-08-14 17:41:33 -0500

    First commit

---
+++ ChrescoTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ChrescoTest
+//
+//  Created by miguel on 8/14/18.
+//  Copyright © 2018 Samtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //test changue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
